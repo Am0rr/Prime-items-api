af65634356d1c2bf1a11fcfcebf37786b4763c1a
ArtemBereznii <artem.berezniy8@gmail.com>
2026-05-12 15:01:50 +0300

    feat(BLL): implement UpdateProductRequest DTO

---
+++ PI.BLL/DTOs/Catalog/UpdateProductRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace PI.BLL.DTOs.Catalog;
+
+public record UpdateProductRequest
+{
+    public Guid Id { get; init; }
+    public Guid CategoryId { get; init; }
+    public string Name { get; init; } = null!;
+    public string Description { get; init; } = null!;
+    public decimal Price { get; init; }
+    public int StockQuantity { get; init; }
+    public string? ImageUrl { get; init; }
+}
