7996fa0499c6c31adbf0c36b1efc2a4be95f19d9
ArtemBereznii <artem.berezniy8@gmail.com>
2026-05-12 15:41:26 +0300

    feat(BLL): implement CategoryResponse DTO

---
+++ PI.BLL/DTOs/Catalog/CategoryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace PI.BLL.DTOs.Catalog;
+
+public record CategoryResponse
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; } = null!;
+    public string Description { get; init; } = null!;
+}
