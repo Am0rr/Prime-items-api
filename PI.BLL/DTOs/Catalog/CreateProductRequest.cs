494f745e3b363d83b1864370c0f68bc1b0d20edd
ArtemBereznii <artem.berezniy8@gmail.com>
2026-05-12 15:17:41 +0300

    refactor(BLL): delete { get; init; } from Update and Create DTOs

--- PI.BLL/DTOs/Catalog/CreateProductRequest.cs
+++ PI.BLL/DTOs/Catalog/CreateProductRequest.cs
@@ -1,11 +1,10 @@
 ﻿namespace PI.BLL.DTOs.Catalog;
 
-public record CreateProductRequest
-{
-    public Guid CategoryId { get; init; }
-    public string Name { get; init; } = null!;
-    public string Description { get; init; } = null!;
-    public decimal Price { get; init; }
-    public int StockQuantity { get; init; }
-    public string? ImageUrl { get; init; }
-}
+public record CreateProductRequest(
+    Guid CategoryId,
+    string Name,
+    string Description,
+    decimal Price,
+    int StockQuantity,
+    string? ImageUrl
+);
--- PI.BLL/DTOs/Catalog/UpdateProductRequest.cs
+++ PI.BLL/DTOs/Catalog/UpdateProductRequest.cs
@@ -1,12 +1,11 @@
 ﻿namespace PI.BLL.DTOs.Catalog;
 
-public record UpdateProductRequest
-{
-    public Guid Id { get; init; }
-    public Guid CategoryId { get; init; }
-    public string Name { get; init; } = null!;
-    public string Description { get; init; } = null!;
-    public decimal Price { get; init; }
-    public int StockQuantity { get; init; }
-    public string? ImageUrl { get; init; }
-}
+public record UpdateProductRequest(
+    Guid Id,
+    Guid CategoryId,
+    string Name,
+    string Description,
+    decimal Price,
+    int StockQuantity,
+    string? ImageUrl
+);
