0154f4e2869bae86d1c51709c40c9bb6484bffff
ArtemBereznii <artem.berezniy8@gmail.com>
2026-05-13 14:06:37 +0300

    feat(BLL): create IProductService interface

---
+++ PI.BLL/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using PI.BLL.DTOs.Catalog;
+using PI.DAL.Models;
+
+namespace PI.BLL.Interfaces;
+
+public interface IProductService
+{
+    Task<Guid> CreateAsync(CreateProductRequest request);
+    Task UpdateAsync(UpdateProductRequest request);
+    Task DeleteAsync(Guid id);
+    Task<ProductResponse?> GetByIdAsync(Guid id);
+    Task<List<ProductResponse>> GetAllAsync();
+    Task<(IEnumerable<ProductResponse> Items, int TotalCount)> GetPagedAsync(ProductFilterParams filter);
+}
