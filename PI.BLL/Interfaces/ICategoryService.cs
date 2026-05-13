d57634837e14ee5f86884bbf63f02ef558630a4f
ArtemBereznii <artem.berezniy8@gmail.com>
2026-05-13 14:12:30 +0300

    feat(BLL): create ICategoryService interface

---
+++ PI.BLL/Interfaces/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using PI.BLL.DTOs.Catalog;
+
+namespace PI.BLL.Interfaces;
+
+public interface ICategoryService
+{
+    Task<Guid> CreateAsync(CreateCategoryRequest request);
+    Task UpdateAsync(UpdateCategoryRequest request);
+    Task DeleteAsync(Guid id);
+    Task<CategoryResponse?> GetByIdAsync(Guid id);
+    Task<List<CategoryResponse>> GetAllAsync();
+}
