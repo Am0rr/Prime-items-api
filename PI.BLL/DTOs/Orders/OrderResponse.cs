d9fb7c97f406bce8d1d5b503f9d97403c32063f1
temandro <9128753@stud.kai.edu.ua>
2026-05-15 16:23:55 +0300

    feat(BLL): implement OrderResponse DTO (#96)

---
+++ PI.BLL/DTOs/Orders/OrderResponse.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace PI.BLL.DTOs.Orders;
+
+public record OrderResponse
+{
+    public Guid Id { get; init; }
+    public Guid UserId { get; init; }
+    public string Status { get; init; } = string.Empty;
+    public decimal TotalAmount { get; init; }
+    public List<OrderItemResponse> Items { get; init; } = new();
+}
