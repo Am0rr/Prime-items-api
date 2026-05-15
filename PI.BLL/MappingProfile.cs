727a67e54ffe31d62b81f88e4b1a0b418e7fc009
temandro <9128753@stud.kai.edu.ua>
2026-05-15 18:10:49 +0300

    feat(BLL): configure AutoMapper profiles for Order and OrderItem (#105)

--- PI.BLL/MappingProfile.cs
+++ PI.BLL/MappingProfile.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using PI.DAL.Entities.Catalog;
+using PI.DAL.Entities.Orders;
 using PI.BLL.DTOs.Catalog;
+using PI.BLL.DTOs.Orders;
 
 namespace PI.BLL;
 
@@ -12,5 +14,13 @@ public class MappingProfile : Profile
             .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
 
         CreateMap<Category, CategoryResponse>();
+
+        CreateMap<OrderItem, OrderItemResponse>()
+            .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name))
+            .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.Product.ImageUrl))
+            .ForMember(dest => dest.TotalPrice, opt => opt.MapFrom(src => src.Quantity * src.UnitPrice));
+
+        CreateMap<Order, OrderResponse>()
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
     }
 }
