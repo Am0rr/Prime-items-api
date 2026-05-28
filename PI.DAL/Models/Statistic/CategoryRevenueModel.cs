efeb84c41b71743ff2164dc14b78fab2fd8c49db
ArtemBereznii <artem.berezniy8@gmail.com>
2026-05-28 19:31:46 +0300

    feat(DAL): implement CategoryRevenueModel

---
+++ PI.DAL/Models/Statistic/CategoryRevenueModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PI.DAL.Models.Statistic;
+
+public class CategoryRevenueModel
+{
+    public string CategoryName { get; set; } = string.Empty;
+    public decimal TotalRevenue { get; set; }
+}
