d22f0018087d6c52235ab0c385bd839d62f71ed4
ArtemBereznii <artem.berezniy8@gmail.com>
2026-05-30 17:07:03 +0300

    feat(BLL): implement all needed Response DTOs

---
+++ PI.BLL/DTOs/Statistic/StatisticsDTOs.cs
@@ -0,0 +1,31 @@
+﻿namespace PI.BLL.DTOs.Statistic;
+
+public record CategoryRevenueResponse(
+    string CategoryName,
+    decimal TotalRevenue
+);
+
+public record LowStockResponse(
+    Guid ProductId,
+    string Name,
+    int StockQuantity
+);
+
+public record TopUserResponse(
+    Guid UserId,
+    string Email,
+    decimal TotalSpent,
+    int TotalOrders
+);
+
+public record TopProductResponse(
+    Guid ProductId,
+    string Name,
+    int TotalItemsSold,
+    decimal TotalRevenue
+);
+
+public record SummaryResponse(
+    decimal TotalRevenue,
+    int TotalOrders
+);
