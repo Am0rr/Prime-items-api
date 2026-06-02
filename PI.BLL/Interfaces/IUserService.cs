ddf3c493bb2b0617928180aa5bffe6cb545378c8
Am0rr <itsaizel468@gmail.com>
2026-06-02 15:32:30 +0300

    refactor(BLL): optimize UserService (#236)

--- PI.BLL/Interfaces/IUserService.cs
+++ PI.BLL/Interfaces/IUserService.cs
@@ -7,7 +7,7 @@ public interface IUserService
     Task<UserResponse> CreateAsync(CreateUserRequest request, CancellationToken cancellationToken = default);
     Task UpdateAsync(UpdateUserRequest request, CancellationToken cancellationToken = default);
     Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<UserResponse?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<UserResponse?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
-    Task<List<UserResponse>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<UserResponse> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<UserResponse> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+    Task<IEnumerable<UserResponse>> GetAllAsync(CancellationToken cancellationToken = default);
 }
