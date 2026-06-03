9357876a7b518c7eb12546103d030a4a625115a1
temandro <9128753@stud.kai.edu.ua>
2026-06-03 16:04:07 +0300

    fix(BLL): restore default CancellationToken in IOrderService

--- PI.BLL/Interfaces/IOrderService.cs
+++ PI.BLL/Interfaces/IOrderService.cs
@@ -4,15 +4,15 @@ namespace PI.BLL.Interfaces;
 
 public interface IOrderService
 {
-    Task<OrderResponse> CreateAsync(CreateOrderRequest request, Guid userId, CancellationToken cancellationToken);
+    Task<OrderResponse> CreateAsync(CreateOrderRequest request, Guid userId, CancellationToken cancellationToken = default);
 
-    Task UpdateStatusAsync(Guid id, UpdateOrderStatusRequest request, CancellationToken cancellationToken);
+    Task UpdateStatusAsync(Guid id, UpdateOrderStatusRequest request, CancellationToken cancellationToken = default);
 
-    Task DeleteAsync(Guid orderId, CancellationToken cancellationToken);
+    Task DeleteAsync(Guid orderId, CancellationToken cancellationToken = default);
 
-    Task<OrderResponse> GetByIdAsync(Guid orderId, CancellationToken cancellationToken);
+    Task<OrderResponse> GetByIdAsync(Guid orderId, CancellationToken cancellationToken = default);
 
-    Task<IEnumerable<OrderResponse>> GetAllAsync(CancellationToken cancellationToken);
+    Task<IEnumerable<OrderResponse>> GetAllAsync(CancellationToken cancellationToken = default);
 
-    Task<IEnumerable<OrderResponse>> GetUserOrdersAsync(Guid userId, CancellationToken cancellationToken);
+    Task<IEnumerable<OrderResponse>> GetUserOrdersAsync(Guid userId, CancellationToken cancellationToken = default);
 }
